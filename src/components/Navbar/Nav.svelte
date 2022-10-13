5219a9232bc7d36213dbd6c91905cb784e375d6c
appithe <joelaviles413@gmail.com>
2022-10-13 15:34:23 -0500

    Fixed z-index of navbar

--- src/components/Navbar/Nav.svelte
+++ src/components/Navbar/Nav.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<nav class="bg-magic-mint w-screen h-[150px] border-2 flex flex-row gap-40 justify-center items-center sticky top-0">
+<nav class="bg-magic-mint w-screen h-[150px] border-2 flex flex-row gap-40 justify-center items-center sticky top-0 z-30">
 	<slot />
 </nav>
 
