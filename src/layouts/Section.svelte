7102b3370f1bb0e27265eeefaaf674ac42f2511d
appithe <joelaviles413@gmail.com>
2022-10-12 23:45:03 -0500

    Added all sections files to start developing

---
+++ src/layouts/Section.svelte
@@ -0,0 +1,12 @@
+<script>
+
+	export let bgColor="";
+	export let svgWaveBottom="";
+	export let sectionId="";
+
+</script>
+
+<section id={sectionId} class="{bgColor} w-screen h-[709px] text-magic-mint border-black border-2">
+	  <slot />
+	  <img src={svgWaveBottom} alt="wave" class="bottom-full w-full" />
+</section>
