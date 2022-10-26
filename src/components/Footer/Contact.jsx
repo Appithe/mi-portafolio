ea2c65dca9312b835962d12cdfed13ca7959dc6a
appithe <joelaviles413@gmail.com>
2022-10-25 20:40:29 -0500

    added animation to border-b  and open new tab when click link

--- src/components/Footer/Contact.jsx
+++ src/components/Footer/Contact.jsx
@@ -6,9 +6,9 @@ export const Contact = (props) => {
 	const { icon, message, link } = props;
 
 	return (
-		<a href={link} className='flex flex-row items-center gap-5 mb-2 h-8 w-fit'>
+		<a href={link} target='_blank' rel='noreferrer' className='group flex flex-row items-center gap-5 mb-2 h-8 w-fit'>
 			<img src={icon} alt={message} className='h-8 w-8' />
-			<span className='font-Libre text-span border-b-2 border-transparent hover:border-b-2 hover:border-blue-500'>{message}</span>
+			<span className='font-Libre text-span border-b-2 border-transparent group-hover:after:border-b-2 group-hover:border-blue-500 group-hover:transition group-hover:duration-300 group-hover:ease-in'>{message}</span>
 		</a>
 	);
 };
