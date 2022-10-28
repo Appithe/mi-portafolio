5d8bbf04c8f167494f2f01f8bbce3404db539bee
appithe <joelaviles413@gmail.com>
2022-10-27 22:23:50 -0500

    Refactor Skills section

--- src/components/Skills/SkillSetBody.jsx
+++ src/components/Skills/SkillSetBody.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { SkillSetIcons } from './SkillSetIcons';
+
 export const SkillSetBody = (props) => {
 
-	const { message = 'Hola mundo' } = props;
+	const { message, title, icons } = props;
 
 	return (
-		<p className='text-xl font-Libre'>{message}</p>
+		<>
+			<div className="flex flex-row items-center gap-2">
+				<h3 className="text-h3 font-Fjalla">{title}</h3>
+				<SkillSetIcons icons={icons} />
+			</div>
+			<p className='text-xl font-Libre'>{message}</p>
+		</>
 	);
 };
 
 SkillSetBody.propTypes = {
 	message: PropTypes.string,
+	title: PropTypes.string,
+	icons: PropTypes.array,
 };
