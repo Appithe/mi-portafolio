aa5c9eb4c84136de1933c8f33cf6cef9453ca9b6
appithe <joelaviles413@gmail.com>
2022-11-01 14:46:12 -0600

    delete todo & fix identation of section component

--- src/components/MyWork/MyWorkHeader.svelte
+++ src/components/MyWork/MyWorkHeader.svelte
@@ -7,6 +7,5 @@
 </script>
 
 <div class="grid grid-cols-12 gap-5">
-	<!-- TODO: Implement store contract -->
     <h2 class="text-h2 font-Fjalla pb-10 col-start-2 col-span-2">{ projects.header }</h2>
 </div>
